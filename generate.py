56541eb8ac868fcf39998a7c1ae3f668a26b20aa
simon gao <little51@126.com>
2023-07-28 23:27:31 +0800

    fiexed generate.py

--- generate.py
+++ generate.py
@@ -1,61 +1,61 @@
 import os
 import sys
-import argparse
 
+import argparse
 import torch
 import transformers
 from peft import PeftModel
 from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
+
+from utils.prompter import Prompter
 
 if torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
 
-generation_config = dict(
-    temperature=0.2,
-    top_k=40,
-    top_p=0.9,
-    do_sample=True,
-    num_beams=1,
-    repetition_penalty=1.3,
-    max_new_tokens=100
-)
+try:
+    if torch.backends.mps.is_available():
+        device = "mps"
+except:  # noqa: E722
+    pass
 
-if __name__ == '__main__':
-    # parse args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--base_model', default=None, type=str, required=True)
-    parser.add_argument('--lora_weights', default=None, type=str,
-                        help="If None, perform inference on the base model")
-    parser.add_argument('--load_8bit', action='store_true',
-                        help='only use CPU for inference')
-    args = parser.parse_args()
-    if args.load_8bit is None:
-        load_8bit = False
-    else:
-        load_8bit = args.load_8bit
-    if args.base_model is None:
-        base_model = "./model/llama-7b"
-    else:
-        base_model = args.base_model
-    if args.lora_weights is None:
-        lora_weights = "./model/llama-peft"
-    else:
-        lora_weights = args.lora_weights
 
-    # load model
+def main(
+    load_8bit: bool = False,
+    base_model: str = "",
+    lora_weights: str = "tloen/alpaca-lora-7b",
+    prompt_template: str = ""
+):
+    base_model = base_model or os.environ.get("BASE_MODEL", "")
+    assert (
+        base_model
+    ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
+
+    prompter = Prompter(prompt_template)
     tokenizer = LlamaTokenizer.from_pretrained(base_model)
     if device == "cuda":
         model = LlamaForCausalLM.from_pretrained(
             base_model,
             load_in_8bit=load_8bit,
             torch_dtype=torch.float16,
-            device_map="auto"
+            device_map="auto",
         )
         model = PeftModel.from_pretrained(
             model,
             lora_weights,
+            torch_dtype=torch.float16,
+        )
+    elif device == "mps":
+        model = LlamaForCausalLM.from_pretrained(
+            base_model,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
             torch_dtype=torch.float16,
         )
     else:
@@ -68,31 +68,74 @@ if __name__ == '__main__':
             device_map={"": device},
         )
 
-    model.config.pad_token_id = tokenizer.pad_token_id = 0
+    # unwind broken decapoda-research config
+    model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
     model.config.bos_token_id = 1
     model.config.eos_token_id = 2
 
     if not load_8bit:
-        model.half()
+        model.half()  # seems to fix bugs for some users.
 
     model.eval()
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
 
-    # generate
-    while True:
-        input_text = input("Input:")
-        if len(input_text.strip()) == 0:
-            break
-        inputs = tokenizer(input_text, return_tensors="pt")
-        generation_output = model.generate(
-            input_ids=inputs["input_ids"].to(device),
-            attention_mask=inputs['attention_mask'].to(device),
-            eos_token_id=tokenizer.eos_token_id,
-            pad_token_id=tokenizer.pad_token_id,
-            **generation_config
+    def evaluate(
+        instruction,
+        input=None,
+        temperature=0.1,
+        top_p=0.75,
+        top_k=40,
+        num_beams=4,
+        max_new_tokens=128,
+        stream_output=False,
+        **kwargs,
+    ):
+        prompt = prompter.generate_prompt(instruction, input)
+        inputs = tokenizer(prompt, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(device)
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            num_beams=num_beams,
+            **kwargs,
         )
-        s = generation_output[0]
-        response = tokenizer.decode(s, skip_special_tokens=True)
-        print("Response: ", response)
-        print("\n")
+
+        generate_params = {
+            "input_ids": input_ids,
+            "generation_config": generation_config,
+            "return_dict_in_generate": True,
+            "output_scores": True,
+            "max_new_tokens": max_new_tokens,
+        }
+
+        with torch.no_grad():
+            generation_output = model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=max_new_tokens,
+            )
+        s = generation_output.sequences[0]
+        output = tokenizer.decode(s)
+        return prompter.get_response(output)
+
+    while True:
+        instruction = input("Input:")
+        if len(instruction.strip()) == 0:
+            break
+        print("Response:", evaluate(instruction))
+
+
+if __name__ == "__main__":
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_model', default=None, type=str, required=True)
+    parser.add_argument('--lora_weights', default=None, type=str,
+                        help="If None, perform inference on the base model")
+    parser.add_argument('--load_8bit', action='store_true',
+                        help='only use CPU for inference')
+    args = parser.parse_args()
+    main(args.load_8bit, args.base_model, args.lora_weights, "")
